e8cc21150cdf19fa016dc7e7a5a1202da3e50ca4
Saroj Bhattarai <sarojbhattarai800@gmail.com>
2020-06-08 19:31:07 +0545

    Able to fetch from database (PUT DELETE POST GET)

--- movie/routes/genres.js
+++ movie/routes/genres.js
@@ -1,64 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const genres = [
-  { id: 1, name: "Thriller" },
-  { id: 2, name: "Action" },
-  { id: 3, name: "Fantasy" },
-  { id: 4, name: "Sci-Fi" },
-];
-
-router.get("/", (req, res) => {
-  res.send(genres);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
 });
 
-router.post("/", (req, res) => {
+const Genre = mongoose.model("Genre", genreSchema);
+
+router.get("/", async (req, res) => {
+  const genre = await Genre.find().sort("name");
+  res.send(genre);
+});
+
+router.post("/", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
-
-  const genre = {
-    id: genres.length + 1,
+  let genre = new Genre({
     name: req.body.name,
-  };
-  genres.push(genre);
-  res.send(genres);
-});
+  });
+  genre = await genre.save().catch(err =>{
+    console.log("Error while posting", err)
+  }); 
 
-router.get("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre) {
-    res.status(404).send("Error 404 on get");
-  }
   res.send(genre);
 });
 
-
-router.put("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
   if (!genre) {
-    return res.status(404).send("Error 404 on PUT");
-  }
+    res.status(404).send("Error 404 on get");
+  } 
+  res.send(genre);
+});
 
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) {
     return res.status(404).send(error.details[0].message);
   }
-  genre.name = req.body.name;
+
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!genre) {
+    return res.status(404).send("Error 404 on PUT");
+  }
   res.send(genre);
 });
 
-router.delete("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre) {
     return res.status(404).send("Error 404 on delete");
   }
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
   res.send(genre);
 });
 
