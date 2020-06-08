2f4111ae8762e18e410a78f41c6863a1edca939f
Saroj Bhattarai <sarojbhattarai800@gmail.com>
2020-06-08 19:33:16 +0545

    mongoose

--- movie/index.js
+++ movie/index.js
@@ -1,10 +1,23 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const Joi = require('joi');
+const Joi = require("joi");
 const app = express();
 const genres = require("./routes/genres");
 app.use(express.json());
 
 app.use("/routes/genres", genres);
+
+mongoose
+  .connect("mongodb://localhost/movies", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
