5559487abf7b2c0ab02bb628bf5b8320e352ea7e
Saroj Bhattarai <sarojbhattarai800@gmail.com>
2020-06-13 21:24:44 +0545

    user model is defined

---
+++ movie/models/users.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 100,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 2048,
+  },
+});
+
+const User = mongoose.model("User", User);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(3).max(100).required(),
+    password: Joi.string().min(6).max(2048).required(),
+  };
+  return Joi.validate(user, schema);
+}
+
+module.exports.User = User;
+module.exports.validate = validateUser;
