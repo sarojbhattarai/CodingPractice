2172a8d1960da030ac9b45616ea53dcb1f23b8a4
= <sarojbhattarai800@gmail.com>
2020-06-18 22:36:38 +0545

    getting individual users

--- movie/routes/users.js
+++ movie/routes/users.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");//auth is authorisation
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const config = require("config");
@@ -6,6 +7,12 @@ const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
+
+
+router.get("/me",auth, async(req,res)=>{
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
