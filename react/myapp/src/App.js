ccd8103e5abbe7d203e9a47cbb78b5ef3b3713a0
= <sarojbhattarai800@gmail.com>
2020-07-02 22:21:43 +0545

    Decrement button and stuffs

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -34,6 +34,14 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -45,6 +53,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
